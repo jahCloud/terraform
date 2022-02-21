d7b28fa712de6f436dd6a4451f88a1cadb18ee8a
jahCloud <83871251+jahCloud@users.noreply.github.com>
2022-02-22 00:57:57 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "cidr_subnet" {
 
 variable "availability_zone" {
   description = "availability zone to create subnet"
-  default     = "us-east-2a"
+  default     = "eu-west-1a"
 }
 
 variable "public_key_path" {
